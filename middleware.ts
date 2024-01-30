5b3154db6a07ea6bcf752063075812da19dcaa6d
joe-shajan <joeshajan1551@gmail.com>
2024-01-31 03:12:40 +0530

    fix: redirect login

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ export default withAuth(function middleware(req) {}, {
   callbacks: {
     authorized: ({ req, token }) => {
       if (req.nextUrl.pathname.startsWith("/") && token === null) {
-        return true;
+        return false;
       }
       return true;
     },
