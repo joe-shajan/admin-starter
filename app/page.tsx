40bd6cb99cab75c42f4ac43df114261f708ece49
joe-shajan <joeshajan1551@gmail.com>
2024-01-31 21:41:06 +0530

    feat: user role

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-import { SectionForm } from "@/components/sectionForm";
-import { useState } from "react";
-import { Sections as SctionTYpe } from "@/types";
 import { Sections } from "@/components/sections";
 
 export default function Home() {
-  const [selectedSection, setSelectedSection] = useState<SctionTYpe | null>(
-    null
-  );
-
   return (
     <div className="flex h-[91vh] p-2 gap-2">
       <Sections hide={false} />
