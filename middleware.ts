b3ac6a3078c10fe9c19f0f45dfa8a3616559dc97
joe-shajan <joeshajan1551@gmail.com>
2024-02-23 18:31:03 +0530

    fix: no auth

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,8 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(function middleware(req) {}, {
   callbacks: {
     authorized: ({ req, token }) => {
-      if (req.nextUrl.pathname.startsWith("/api") && token === null) {
-        return false;
-      }
       if (req.nextUrl.pathname.startsWith("/") && token === null) {
-        return false;
+        return true;
       }
       return true;
     },
