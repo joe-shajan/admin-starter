b7ff1139c41340ad39d2ab856b0a2281e5d4a2c1
joe-shajan <joeshajan1551@gmail.com>
2024-02-23 18:07:00 +0530

    make api public

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,9 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(function middleware(req) {}, {
   callbacks: {
     authorized: ({ req, token }) => {
+      if (req.nextUrl.pathname.startsWith("/api") && token === null) {
+        return false;
+      }
       if (req.nextUrl.pathname.startsWith("/") && token === null) {
         return false;
       }
