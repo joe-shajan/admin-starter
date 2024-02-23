0d51431514e496ad9e5fb1f1d02eeccd294471d3
joe-shajan <joeshajan1551@gmail.com>
2024-02-23 18:17:05 +0530

    allow access

--- next.config.js
+++ next.config.js
@@ -6,19 +6,26 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
+        // Routes this applies to
+        source: "/api/(.*)",
+        // Headers
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          // Allow for specific domains to have access or * for all
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+            // DOES NOT WORK
+            // value: process.env.ALLOWED_ORIGIN,
+          },
+          // Allows for specific methods accepted
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
           },
+          // Allows for specific headers accepted (These are a few standard ones)
           {
             key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            value: "Content-Type, Authorization",
           },
         ],
       },
